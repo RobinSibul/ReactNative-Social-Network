c296e1643d1e6cc2aec6c32d56d824c04d316834
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2022-12-30 18:56:42 +0200

    Update styles.js

--- src/screens/Auth/RegistrationScreen/styles.js
+++ src/screens/Auth/RegistrationScreen/styles.js
@@ -1,3 +1,13 @@
 import { StyleSheet } from "react-native";
 
-export const styles = {};
+export const styles = StyleSheet.create({
+  linkPath: {
+    marginTop: 16,
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "400",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+});
