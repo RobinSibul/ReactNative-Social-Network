e55e6f4d5211cd3ca92250d6728eaff2e060fa60
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2022-12-31 01:58:58 +0200

    Create PostsList.js

---
+++ src/shared/components/PostsList/PostsList.js
@@ -0,0 +1,31 @@
+import { StyleSheet, View, FlatList } from "react-native";
+
+import PostItem from "../PostItem/PostItem";
+
+export default function PostsList({ posts }) {
+  return (
+    <View style={styles.postsWrapper}>
+      <FlatList
+        data={posts}
+        keyExtractor={({ id }) => id}
+        renderItem={({ item }) => (
+          <PostItem
+            id={item.id}
+            photo={item.photo}
+            name={item.name}
+            locationName={item.locationName}
+            locationCoords={item.locationCoords}
+            comments={item.comments}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  postsWrapper: {
+    width: "100%",
+    paddingTop: 32,
+  },
+});
