e948c3f81ce9d6efc4a81f14f5eca9e0850ce8f4
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2022-12-30 18:43:11 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useCallback } from "react";
+import "expo-dev-menu";
+import { Text } from "react-native";
+
+import { LogBox } from "react-native";
+import { Provider } from "react-redux";
+
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+import { store } from "./src/redux/store";
+import Main from "./src/screens/MainScreens/Main";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto: require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  LogBox.ignoreLogs(["EventEmitter.removeListener"]);
+
   return (
-    <View style={styles.container}>
-      <Text>Bla</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Main onLayoutRootView={onLayoutRootView} />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
