a9142ca8bdfa388fc88c256dc29968e1ff26baad
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2022-12-31 01:59:09 +0200

    Update Home.js

--- src/screens/NestedScreens/Home/Home.js
+++ src/screens/NestedScreens/Home/Home.js
@@ -1,1 +1,57 @@
-export default function Home() {}
+import { useState, useEffect, useLayoutEffect } from "react";
+import { useDispatch } from "react-redux";
+import { TouchableOpacity } from "react-native";
+
+import useAuth from "../../../shared/hooks/useAuth";
+
+import { authSignOut } from "../../../redux/auth/auth-operation";
+// import { fetchPosts } from "../../../shared/api/api-posts";
+
+import UserInfo from "./UserInfo/UserInfo";
+import Container from "../../../shared/components/Container/Container";
+import PostsList from "../../../shared/components/PostsList/PostsList";
+import Icon from "../../../shared/components/Icon/Icon";
+
+import { arrayPosts } from "./arrayPost";
+
+export default function Home({ route, navigation }) {
+  const dispatch = useDispatch();
+  const { user } = useAuth();
+  const { email, login, photoURL: uri } = user;
+
+  const { navigate } = navigation;
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{ paddingRight: 10 }}
+          activeOpacity={0.8}
+          onPress={() => {
+            dispatch(authSignOut());
+          }}
+        >
+          <Icon type="logout" focused={false} size="25" />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
+  useEffect(() => {
+    // fetchPosts(comments, setPosts, setComments);
+
+    return () => {
+      setComments([]);
+      setPosts([]);
+    };
+  }, []);
+
+  return (
+    <Container>
+      <UserInfo uri={uri} login={login} email={email} />
+      <PostsList posts={arrayPosts} />
+    </Container>
+  );
+}
