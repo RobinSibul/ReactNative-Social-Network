6b5b575ae3145ef98155b53718e56d74466cf019
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2022-12-31 01:59:16 +0200

    Update PostsScreen.js

--- src/screens/MainScreens/PostsScreen.js
+++ src/screens/MainScreens/PostsScreen.js
@@ -1,5 +1,34 @@
-import { Text } from "react-native";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "../NestedScreens/Home/Home";
+import CommentsScreen from "../NestedScreens/CommentsScreen/CommentsScreen";
+import MapScreen from "../NestedScreens/MapScreen";
+
+import Icon from "../../shared/components/Icon/Icon";
+
+const NestedScreen = createStackNavigator();
 
 export default function PostsScreen() {
-  return <Text>PostsScreen</Text>;
+  return (
+    <NestedScreen.Navigator initialRouteName={"Публикации"} headerShown={false}>
+      <NestedScreen.Screen
+        name="Публікації"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Icon type="grid" focused={focused} size={focused ? 40 : 40} />
+          ),
+        }}
+      />
+      <NestedScreen.Screen
+        name="Коментарі"
+        component={CommentsScreen}
+        options={{
+          tabBarStyle: { display: "none" },
+        }}
+      />
+      <NestedScreen.Screen name="Карта" component={MapScreen} />
+    </NestedScreen.Navigator>
+  );
 }
