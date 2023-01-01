89262c58a2589ef825988e4ba27433f3067b7a58
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-01 23:48:40 +0200

    Update icons.js

--- src/shared/components/Icon/icons.js
+++ src/shared/components/Icon/icons.js
@@ -17,9 +17,7 @@ export const deleteIcon = (focused) => {
 export const commentIcon = (focused) => {
   const color = !focused ? "#E8E8E8" : "#FF6C00";
   return `<svg viewBox="0 0 24 24" fill="none">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M3 11.5C2.99656 12.8199 3.30493 14.1219 3.9 15.3C5.33904 18.1793 8.28109 19.9988 11.5 20C12.8199 20.0034 14.1219 19.6951 15.3 19.1L21 21L19.1 15.3C19.6951 14.1219 20.0034 12.8199 20 11.5C19.9988 8.28109 18.1793 5.33904 15.3 3.9C14.1219 3.30493 12.8199 2.99656 11.5 3H11C6.68419 3.2381 3.2381 6.68419 3 11V11.5V11.5Z" stroke="${color}" fill="${
-    focused && color
-  }" stroke-linecap="round" stroke-linejoin="round"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M3 11.5C2.99656 12.8199 3.30493 14.1219 3.9 15.3C5.33904 18.1793 8.28109 19.9988 11.5 20C12.8199 20.0034 14.1219 19.6951 15.3 19.1L21 21L19.1 15.3C19.6951 14.1219 20.0034 12.8199 20 11.5C19.9988 8.28109 18.1793 5.33904 15.3 3.9C14.1219 3.30493 12.8199 2.99656 11.5 3H11C6.68419 3.2381 3.2381 6.68419 3 11V11.5V11.5Z" stroke="${color}" fill="${color}" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 `;
 };
