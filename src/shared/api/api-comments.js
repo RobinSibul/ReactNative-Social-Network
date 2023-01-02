06ed8b282f02bcdf788b4d55eed872bba072cf45
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-02 02:16:08 +0200

    Create api-comments.js

---
+++ src/shared/api/api-comments.js
@@ -0,0 +1,46 @@
+import db from "../../firebase/config";
+
+export async function fetchPostComments(setComments) {
+  try {
+    await db
+      .firestore()
+      .collection("posts")
+      .doc(id)
+      .collection("comments")
+      .onSnapshot(({ docs }) => {
+        setComments(
+          docs
+            .map((doc) => ({
+              ...doc.data(),
+              id: doc.id,
+            }))
+            .sort((firstPost, lastPost) => lastPost.dateID - firstPost.dateID)
+        );
+      });
+  } catch (error) {
+    console.log(
+      `%c[Error - fetchPostComments(): ${error.message}]`,
+      "color: #F44336;"
+    );
+
+    throw error;
+  }
+}
+export async function handleComment(setComment) {
+  try {
+    await db
+      .firestore()
+      .collection("posts")
+      .doc(id)
+      .collection("comments")
+      .add({ comment, login, date, dateID: Date.now(), photoURL });
+    setComment("");
+  } catch (error) {
+    console.log(
+      `%c[Error - handleComment(): ${error.message}]`,
+      "color: #F44336;"
+    );
+
+    throw error;
+  }
+}
