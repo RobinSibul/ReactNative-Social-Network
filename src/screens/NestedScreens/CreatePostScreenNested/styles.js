4fa5b9dbe9fbea0fa4c4d64cf9c7373691a60cd8
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-03 17:38:51 +0200

    Update styles.js

--- src/screens/NestedScreens/CreatePostScreenNested/styles.js
+++ src/screens/NestedScreens/CreatePostScreenNested/styles.js
@@ -1,3 +1,74 @@
 import { StyleSheet } from "react-native";
 
-export const styles = {};
+export const styles = StyleSheet.create({
+  imgContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    zIndex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+
+    height: 240,
+    width: "91%",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    marginHorizontal: 16,
+  },
+  cameraContainer: {
+    position: "relative",
+    top: 0,
+    left: 0,
+    zIndex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+
+    height: 240,
+    borderRadius: 8,
+    marginHorizontal: 16,
+  },
+
+  btnPhotoWrapper: {
+    position: "relative",
+
+    backgroundColor: "#FFF",
+    borderRadius: 50,
+    width: 60,
+    height: 60,
+  },
+  icnWrapper: {
+    position: "absolute",
+    top: "30%",
+    left: "30%",
+  },
+  text: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "400",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#BDBDBD",
+
+    marginHorizontal: 16,
+    marginTop: 8,
+  },
+  form: {
+    marginTop: 32,
+    marginHorizontal: 16,
+  },
+  btnTrashContainer: {
+    position: "absolute",
+    right: 0,
+    left: 0,
+
+    alignItems: "center",
+  },
+  btnTrash: {
+    width: 70,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
