d8fcecc333b370814e9bb72e843f6b3e43a8156f
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-03 17:38:41 +0200

    Update styles.js

--- src/screens/NestedScreens/CommentsScreen/styles.js
+++ src/screens/NestedScreens/CommentsScreen/styles.js
@@ -6,7 +6,7 @@ export const styles = StyleSheet.create({
     bottom: 0,
     backgroundColor: "#fff",
     width: "100%",
-    height: 70,
+    minHeight: 70,
 
     paddingHorizontal: 16,
     paddingTop: 5,
@@ -15,6 +15,7 @@ export const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     alignItems: "center",
+
     justifyContent: "space-between",
     width: "100%",
     maxHeight: 50,
@@ -24,22 +25,57 @@ export const styles = StyleSheet.create({
     borderRadius: 100,
     backgroundColor: "#F6F6F6",
 
-    paddingLeft: 16,
-    paddingRight: 8,
+    padding: 16,
+
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.1)",
   },
   txtInput: {
     fontFamily: "Roboto",
     fontSize: 13,
     fontStyle: "normal",
+    width: "80%",
+    flexDirection: "row-reverse",
+    justifyContent: "center",
+    minHeight: 50,
   },
   btnComment: {
     alignItems: "center",
     justifyContent: "center",
     width: 34,
     height: 34,
+
     borderRadius: 50,
     backgroundColor: "#FF6C00",
+
     padding: 0,
     margin: 0,
+  },
+  styleViewInput: {
+    position: "relative",
+    top: 0,
+    bottom: 0,
+    width: "100%",
+    height: "100%",
+
+    backgroundColor: "rgba(255,255,255,0.8)",
+
+    paddingHorizontal: 16,
+    paddingTop: 5,
+  },
+  styleInput: {
+    position: "absolute",
+
+    left: 16,
+    right: 16,
+
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    minHeight: 50,
+
+    paddingLeft: 16,
+    paddingRight: 8,
+    padding: 16,
   },
 });
