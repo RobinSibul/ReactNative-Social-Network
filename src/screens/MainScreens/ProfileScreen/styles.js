8a7509478575aa957d517fc594e005ac2f0ee74e
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-03 17:38:36 +0200

    Update styles.js

--- src/screens/MainScreens/ProfileScreen/styles.js
+++ src/screens/MainScreens/ProfileScreen/styles.js
@@ -1,3 +1,5 @@
 import { StyleSheet } from "react-native";
 
-export const styles = {};
+export const styles = StyleSheet.create({
+  iconWrapper: { position: "absolute", right: -15, bottom: 15 },
+});
