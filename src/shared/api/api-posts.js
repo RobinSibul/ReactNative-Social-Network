e0e073b349d3eb976fe0209c2edf6c331766c420
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-04 02:08:51 +0200

    Update api-posts.js

--- src/shared/api/api-posts.js
+++ src/shared/api/api-posts.js
@@ -1,44 +1,19 @@
 import db from "../../firebase/config";
-
-export async function fetchPosts(comments, setComments, setPosts) {
+export async function fetchPosts(setPosts, setComments, comments) {
   try {
     await db
       .firestore()
       .collection("posts")
       .onSnapshot(({ docs }) => {
-        docs.map((doc) => {
-          db.firestore()
-            .collection("posts")
-            .doc(doc.id)
-            .collection("comments")
-            .onSnapshot(({ docs }) => {
-              const arr = [];
-              docs.map((doc) => arr.push(doc.data()));
-              setComments((prevState) => {
-                return [
-                  ...prevState,
-                  { id: doc.id, commentNumber: docs.length, comments: arr },
-                ];
-              });
-            });
-          comments.map((item) => {
-            setPosts((prevState) => {
-              return [
-                ...prevState,
-                {
-                  id: doc.id,
-                  ...doc.data(),
-                  comment: item.id === doc.id && item.commentNumber,
-                },
-              ]
-                .filter(
-                  (post, indx, arr) =>
-                    post.comment !== false && arr.indexOf(post) === indx
-                )
-                .sort((firstPost, lastPost) => lastPost.date - firstPost.date);
-            });
-          });
-        });
+        setPosts(
+          docs.map((doc) => {
+            fetchingCollection(setComments, doc.id);
+            return {
+              ...doc.data(),
+              id: doc.id,
+            };
+          })
+        );
       });
   } catch (error) {
     console.log(
@@ -50,28 +25,23 @@ export async function fetchPosts(comments, setComments, setPosts) {
   }
 }
 
-export async function getUserPosts(comments, setComments, setPosts, userId) {
+export async function getUserPosts(setComments, setPosts, userId) {
   try {
     await db
       .firestore()
       .collection("posts")
       .where("userID", "==", userId)
       .onSnapshot(({ docs }) => {
-        docs.map((doc) => {
-          fetchingCollection(setComments, doc.id);
-        });
-
-        comments.map((comment) =>
-          setPosts(
-            docs
-              .map((doc) => ({
+        setPosts(
+          docs
+            .map((doc) => {
+              fetchingCollection(setComments, doc.id);
+              return {
                 id: doc.id,
                 ...doc.data(),
-                commentNumber:
-                  doc.id === comment.id ? comment.commentNumber : 0,
-              }))
-              .sort((firstPost, lastPost) => lastPost.date - firstPost.date)
-          )
+              };
+            })
+            .sort((firstPost, lastPost) => lastPost.date - firstPost.date)
         );
       });
   } catch (error) {
@@ -98,6 +68,7 @@ async function fetchingCollection(setComments, id) {
             {
               id,
               commentNumber: docs.length,
+              comments: docs.map((doc) => doc.data()),
             },
           ];
         });
@@ -111,85 +82,3 @@ async function fetchingCollection(setComments, id) {
     throw error;
   }
 }
-
-//TODO
-/* 
-1. Получить массив обьектов Постов. В каждом обьекте Поста  есть массив объектов Комментариев.
-2. Post obj
-  {
-    date (for sort)
-    locationCoords: {
-      latitude,
-      longtitude
-      }
-    locationName
-    login
-    name
-    photo (post image)
-    userID
-    comments: []
-    likes
-  }
-3. Comment obj
-    {
-    comment
-    date (txt)
-    dateID (for sort)
-    login
-    photoURL (of user)
-    }
-  
-4. Создать пост - создать обьект, отправить на базу, добавить в массив локально
-5. Создать коммент - создать обьект, отправить на базу, добавить в массив локально соответственному Посту по id
-6. Profile screen - отбирать с массива Posts - по userId массив постов. 
-*/
-
-/*
-export async function fetchPosts(setPosts) {
-  try {
-    await db
-      .firestore()
-      .collection("posts")
-      .onSnapshot(({ docs }) => {
-        docs.map((doc) => {
-          const comments = fetchComments(doc.id);
-          setPosts((prevState) =>
-            [
-              ...prevState,
-              {
-                ...doc.data(),
-                id: doc.id,
-                comments,
-              },
-            ].sort((firstPost, lastPost) => lastPost.date - firstPost.date)
-          );
-        });
-      });
-  } catch (error) {
-    console.log(
-      `%c[Error - fetchPosts(): ${error.message}]`,
-      "color: #F44336;"
-    );
-    throw error;
-  }
-}
-async function fetchComments(id) {
-  try {
-    const snapshot = await db
-      .firestore()
-      .collection("posts")
-      .doc(id)
-      .collection("comments")
-      .get();
-    const comments = snapshot.docs.map((doc) => ({ ...doc.data() }));
-    return comments;
-  } catch (error) {
-    console.log(
-      `%c[Error - fetchComments(): ${error.message}]`,
-      "color: #F44336;"
-    );
-    throw error;
-  }
-}
-*/
-Footer;
