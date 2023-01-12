ecd4261656a41261771548caf433c58653c80f62
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-12 06:00:28 +0200

    Create colors.js

---
+++ src/shared/constants/colors.js
@@ -0,0 +1,38 @@
+export const LIGHT_COLORS = {
+  background: "#fff",
+  textColor: "#212121",
+  inputTxtColor: "#212121",
+  thumbColor: "#F6F6F6",
+  placeholderColor: "#F6F6F6",
+  links: "#1B4371",
+};
+
+export const DARK_COLORS = {
+  background: "rgb(28, 28, 30)",
+  textColor: "rgb(242, 242, 247)",
+  inputTxtColor: "#212121",
+  thumbColor: "#fff",
+  placeholderColor: "#F6F6F6",
+  links: "#A5C1F9",
+};
+
+/*
+white --> dark
+background: #fff - rgb(255, 255, 255)
+photoBtn: #fff - rgb(255, 255, 255)
+
+darkgrey --> white
+titleColor: #212121 - rgb(33, 33, 33)
+txtColor: #212121 - rgb(33, 33, 33)
+
+LightGrey --> white
+backgroundInput: #F6F6F6 - rgb(246, 246, 246)
+createPhotoWrapper: #F6F6F6 - rgb(246, 246, 246)
+commentWrapper: #F6F6F6 - rgb(246, 246, 246)
+
+grey --> grey
+placeholderColor: #BDBDBD - rgb(189, 189, 189)
+
+для инпута не менять цвет текста
+
+*/
