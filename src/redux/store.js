5e15a80e9c0981dbab7373ee2cfce07722f43129
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-12 05:59:08 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,4 +4,15 @@ import authReducer from "./auth/auth-slice";
 
 export const store = configureStore({
   reducer: authReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: ["auth/signin/fulfilled", "auth/signin/rejected"],
+        // Ignore these field paths in all actions
+        ignoredActionPaths: ["meta.arg", "payload.multiFactor"],
+        // Ignore these paths in the state
+        ignoredPaths: ["error", "payload"],
+      },
+    }),
 });
