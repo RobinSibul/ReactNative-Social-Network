bb98311ed8d14274521eed72662e71de92108876
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-12 05:59:21 +0200

    Update CreatePostScreen.js

--- src/screens/MainScreens/CreatePostScreen.js
+++ src/screens/MainScreens/CreatePostScreen.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import useSwitchTheme from "../../shared/hooks/useSwitchTheme";
 import CreatePostScreenNested from "../NestedScreens/CreatePostScreenNested/CreatePostScreenNested";
 
 const NestedScreen = createStackNavigator();
 
 export default function CreatePostScreen() {
+  const { switcher, colors } = useSwitchTheme({ side: "right" });
+
   return (
     <NestedScreen.Navigator>
       <NestedScreen.Screen
         name="Створити публікацію"
         component={CreatePostScreenNested}
         options={{
+          headerRight: () => switcher,
+          headerTintColor: colors.textColor,
+          headerStyle: { backgroundColor: colors.background },
           tabBarStyle: { display: "none" },
         }}
       />
