9310e4680858ff00a89b79584f53c265c984f5d8
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-18 22:38:02 +0200

    Create ua.js

---
+++ assets/languages/ua.js
@@ -0,0 +1,42 @@
+export const ua = {
+  //register
+  registerTitle: "Реєстрація",
+  login: "Логін",
+  email: "Адреса електронної пошти",
+  password: "Пароль",
+  registerBtn: "Зареєструватися",
+  linkToSignIn: "Вже є аккаунт? Ввійти",
+
+  //login
+  loginTitle: "Ввійти",
+  loginBtn: "Ввійти",
+  linkToSignUp: "Немає аккаунту? Зареєструватися",
+
+  //link Input
+  show: "Показати",
+  close: "Сховати",
+
+  //profile
+  txtAnyPosts: "У вас ще немає постів",
+  txtMakePost: "Зробити пост",
+
+  //postsScreen
+  headerHome: "Публікації",
+  headerComments: "Коментарі",
+  headerMap: "Карта",
+
+  //comments
+  toComment: "Коментувати...",
+
+  //createPost
+  makePost: "Створити публікацію",
+  editPhoto: "Редагувати фото",
+  downloadPhoto: "Завантажити фото",
+  name: "Назва",
+  location: "Місцевість",
+  pushPost: "Опублікувати",
+
+  //makePhoto
+  addPhoto: "Додати",
+  discardPhoto: "Відмінити",
+};
