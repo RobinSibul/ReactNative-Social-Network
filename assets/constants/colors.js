b97f521af86dee474f1187fad0429a67be1b5430
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-18 22:37:51 +0200

    Create colors.js

---
+++ assets/constants/colors.js
@@ -0,0 +1,21 @@
+export const LIGHT_COLORS = {
+  background: "#fff",
+  textColor: "#212121",
+  inputTxtColor: "#212121",
+  thumbColor: "#F6F6F6",
+  placeholderColor: "#F6F6F6",
+  links: "#1B4371",
+  spinnerBackground: "rgba(255,255,255,0.5)",
+  spinner: "rgb(89, 87, 87)",
+};
+
+export const DARK_COLORS = {
+  background: "rgb(28, 28, 30)",
+  textColor: "rgb(242, 242, 247)",
+  inputTxtColor: "#212121",
+  thumbColor: "#fff",
+  placeholderColor: "rgb(209, 209, 209)",
+  links: "#A5C1F9",
+  spinnerBackground: "rgba(28, 28, 30, 0.4)",
+  spinner: "#fff",
+};
