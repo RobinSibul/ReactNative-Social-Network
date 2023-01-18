acb03a54632bd807413a62daf15820b496c6c6ba
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-18 22:38:19 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,17 +4,26 @@ import "firebase/compat/firestore";
 
 import "firebase/compat/storage";
 
+import {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+} from "@env";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDExJWjPmGuMEV09linU-bpCbIuGaIYx6o",
-  authDomain: "rn-p-6a9bd.firebaseapp.com",
-  projectId: "rn-p-6a9bd",
-  storageBucket: "rn-p-6a9bd.appspot.com",
-  messagingSenderId: "148572802526",
-  appId: "1:148572802526:web:510adede381e5968f3a14f",
-  measurementId: "G-HF8W39J1L8",
+  apiKey: `${apiKey}`,
+  authDomain: `${authDomain}`,
+  projectId: `${projectId}`,
+  storageBucket: `${storageBucket}`,
+  messagingSenderId: `${messagingSenderId}`,
+  appId: `${appId}`,
+  measurementId: `${measurementId}`,
 };
 
-// Initialize Firebase
 export default !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
   : firebase.app();
