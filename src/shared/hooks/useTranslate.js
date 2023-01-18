c23f63e7de5b862f93cea7a3eaecf14592297825
dmytroredkinin0710 <dmytroredkinin0710@gmail.com>
2023-01-18 22:38:28 +0200

    Update useTranslate.js

--- src/shared/hooks/useTranslate.js
+++ src/shared/hooks/useTranslate.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 
 import { LanguageContext } from "../providers/LanguageProvider";
 
-import { data } from "../../../languages/dataLanguages";
+import { data } from "../../../assets/languages/dataLanguages";
 
 export default function useTranslate() {
   const { isEn, t, setLangSchema, selected } = useContext(LanguageContext);
